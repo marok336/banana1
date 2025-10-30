911983529a9935734d1b75097dfe5309772da5f6
marok <baran228.b2@gmail.com>
2025-10-30 18:00:52 +0000

    2

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 #///////////1//////////
-name = input("What's your name?: ")
-age = input("How old are you?: ")
-print(f"Привіт {name}, тобі {age}!")
+# name = input("What's your name?: ")
+# age = input("How old are you?: ")
+# print(f"Привіт {name}, тобі {age}!")
+#///////////2//////////
+age = int(input("How old are you?: "))
+if age >= 18:
+    print("Вхід дозволено!")
+else:
+    print("Вхід заборонено!")
