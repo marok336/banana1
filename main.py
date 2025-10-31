c4bf273fa447ec185c696ec3f4a6441471a35db6
marok <baran228.b2@gmail.com>
2025-10-31 21:13:25 +0000

    5

--- main.py
+++ main.py
@@ -20,7 +20,12 @@
 #     else:
 #         print("Correct!")
 #//////////4/////////////
-a = int(input("Введіть перше число: "))
-b = int(input("Введіть друге число: "))
-for i in range(a, b + 1):
-    print(i)
+# a = int(input("Введіть перше число: "))
+# b = int(input("Введіть друге число: "))
+# for i in range(a, b + 1):
+#     print(i)
+#////////////5///////////
+a = int(input("Введiть число: "))
+for i in range(a, 0, -1):
+    if i % 2 == 0:
+        print(i, end=' ')
